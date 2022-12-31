3ef2a69df319f1c48a00e2312023103708415689
Mohib Rehman <Mohibrehman99@gmail.com>
2022-12-31 17:52:12 +0500

    dsa

--- Binance_Socket/Connection/connection.py
+++ Binance_Socket/Connection/connection.py
@@ -1,11 +1,19 @@
 import pymongo
 import pandas as pd
 from datetime import datetime
-client = pymongo.MongoClient("mongodb://root:example@Mongo:27017/?authSource=admin&readPreference=primary&ssl=false&directConnection=true")
+client = pymongo.MongoClient(
+    "mongodb://root:example@Mongo:27017/?authSource=admin&readPreference=primary&ssl=false&directConnection=true")
 db = client.Twigram
 df = pd.read_csv("./shared/Socker.csv")
-df2= df.groupby("Open Time")['Values'].apply(lambda x: '|'.join(x)).reset_index()
-df2 = df2.sort_values(by=['Open Time'],ascending=False)
+df2 = df.groupby("Open Time")['Values'].apply(
+    lambda x: '|'.join(x)).reset_index()
+df2 = df2.sort_values(by=['Open Time'], ascending=False)
 data_dict = df2.to_dict("records")
 db.Twigram_collection.insert_many(data_dict)
-print("Data Inserted at",datetime.now())
+print("Data Inserted at", datetime.now())
+
+# str(datetime.now() + dateutil.relativedelta.relativedelta(months=-1)
+# db.Twigram_collection.deleteMany({Open Time: {"$lt": new Date(2017, 9, 1)}})
+
+
+# ADD LAST UPDATED TIME TO ROOT FOLDER AND THEN PROCESS THE FOLLOWING
