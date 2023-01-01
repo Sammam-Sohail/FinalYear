9d47570e5ca723a8172913979d1632ee487c2988
Mohib Rehman <mohibrehman31@gmail.com>
2023-01-01 12:00:08 +0000

    fdsfsd

--- Binance_Socket/Connection/connection.py
+++ Binance_Socket/Connection/connection.py
@@ -4,13 +4,15 @@ from datetime import datetime
 client = pymongo.MongoClient(
     "mongodb://root:example@Mongo:27017/?authSource=admin&readPreference=primary&ssl=false&directConnection=true")
 db = client.Twigram
-df = pd.read_csv("./shared/Socker.csv")
+df = pd.read_csv("./shared/Socket.csv")
 df2 = df.groupby("Open Time")['Values'].apply(
     lambda x: '|'.join(x)).reset_index()
 df2 = df2.sort_values(by=['Open Time'], ascending=False)
 data_dict = df2.to_dict("records")
 db.Twigram_collection.insert_many(data_dict)
 print("Data Inserted at", datetime.now())
+with open('../shared/Last_Updated.txt', 'w') as f:
+    f.write(str(datetime.now()))
 
 # str(datetime.now() + dateutil.relativedelta.relativedelta(months=-1)
 # db.Twigram_collection.deleteMany({Open Time: {"$lt": new Date(2017, 9, 1)}})
