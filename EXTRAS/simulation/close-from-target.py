7790b63bac12fb0c3537e4cfbe4ebde87a9d155c
mohibrehman31 <mohibrehman31@gmail.com>
2023-01-02 01:30:13 +0500

    das

---
+++ EXTRAS/simulation/close-from-target.py
@@ -0,0 +1,13 @@
+import numpy as np
+import math
+
+targets = [105, 110, 125]
+
+val = 95
+
+
+print([x for i in targets min(val-i)])
+
+# dis = (closest/target1) * 100
+# print(dis, dis2)
+# print(target1-closest, target2-closest2)
