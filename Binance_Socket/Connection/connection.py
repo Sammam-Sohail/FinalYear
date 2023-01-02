f9a7adddbdc4d44ca5254e1fddeb69a763ce6001
mohib <mohibrehman31@gmail.com>
2023-01-02 22:40:22 +0500

    mmdsasd

--- Binance_Socket/Connection/connection.py
+++ Binance_Socket/Connection/connection.py
@@ -1,18 +1,30 @@
 import pymongo
 import pandas as pd
 from datetime import datetime
+import os
 client = pymongo.MongoClient(
     "mongodb://root:example@Mongo:27017/?authSource=admin&readPreference=primary&ssl=false&directConnection=true")
 db = client.Twigram
-df = pd.read_csv("./shared/Socket.csv")
+print(os.getcwd())
+df = pd.read_csv("./shared/Socker.csv")
 df2 = df.groupby("Open Time")['Values'].apply(
     lambda x: '|'.join(x)).reset_index()
 df2 = df2.sort_values(by=['Open Time'], ascending=False)
 data_dict = df2.to_dict("records")
-db.Twigram_collection.insert_many(data_dict)
+db.Binance.insert_many(data_dict)
 print("Data Inserted at", datetime.now())
-with open('../shared/Last_Updated.txt', 'w') as f:
-    f.write(str(datetime.now()))
+
+
+
+for i in os.listdir("./shared/Calls/"):
+    df = pd.read_csv(f"./shared/Calls/{i}/{i}.csv")
+    df.drop(columns=["Cleaned_text"])
+    data_dict = df.to_dict("records")
+    db.Calls.insert_many(data_dict)
+    print("Data Inserted at", datetime.now())
+
+# with open('../shared/Last_Updated.txt', 'w') as f:
+#     f.write(str(datetime.now()))
 
 # str(datetime.now() + dateutil.relativedelta.relativedelta(months=-1)
 # db.Twigram_collection.deleteMany({Open Time: {"$lt": new Date(2017, 9, 1)}})
--- Binance_Socket/Telegram/Channels/binancefuturesignal/binancefuturesignal.py
+++ Binance_Socket/Telegram/Channels/binancefuturesignal/binancefuturesignal.py
@@ -1,56 +1,73 @@
 import pandas as pd
 import re
-colnames=['Name','Date','Time', 'Text','Cleaned_text'] 
-exchanges = ['BINANCE', 'BYBIT', 'FTX', 'HUOBI', 'KUCOIN', 'POLONIEX','KRAKEN']
-df = pd.read_csv('./1_binancefuturesignal.csv',names = colnames,header=None)
-print(df['Cleaned_text'][0])
-print(df['Text'][0])
-df.head()
+import os
+os.chdir("C://Users//Mohib//Desktop//FinalYear//Binance_Socket//Telegram//")
+for Folder_Name in os.listdir(os.path.join(os.getcwd(),"Channels")):
+    if (str(os.path.basename(__file__)).split(".")[0] == Folder_Name):
+        os.chdir(os.path.join(os.getcwd(),"Channels",Folder_Name))
 
-exchanges = 'Binance|kucoin|bitbuy'
-conversion = 'usdt|usd|btc|eth'
-exchangeregex = '|'.join(exchanges)
-target = ('|').join(['1','2','3'])
-# coins=''
-def extractor(i):
-  text = re.sub('[\',\[\]]', '', i)
-  coin_name,conversion_name,call_exchange,call_type,call_duration,leverage,targets,stop_loss= ["None"]*8
-  if(re.search('free signal|entry point',text,re.IGNORECASE) and re.search('target|targets',text,re.IGNORECASE)):    
+        current_path = os.getcwd()
+        Root_PATH = os.path.abspath("../../../")
+        folder_name = os.path.basename(current_path)
 
-    coin_name=re.findall('(\w+){}'.format(conversion),text,re.IGNORECASE)
-    
-    conversion_name = re.findall('\w+({})'.format(conversion),str(text),re.IGNORECASE)
-    
-    """exchange not used"""
-    call_exchange = re.findall('\w+({})'.format(exchanges),text,re.IGNORECASE)
-    
-    call_type = re.findall('sell|buy',text,re.IGNORECASE)
 
-    range =  re.findall(r'around\s+(\d+(?:\.\d+)?\s*-\s*\d+(?:\.\d+)?)',text,re.IGNORECASE)
-    """duration not used that much"""
-    call_duration = re.findall('long|short',text,re.IGNORECASE)
-    """current not given, between is given"""
-    # current_value=re.findall('')
+        colnames=['Name','Date','Time', 'Text','Cleaned_text'] 
+        exchanges = ['BINANCE', 'BYBIT', 'FTX', 'HUOBI', 'KUCOIN', 'POLONIEX','KRAKEN']
+        df = pd.read_csv('./1_binancefuturesignal.csv',names = colnames,header=None)
+        print(df['Cleaned_text'][0])
+        print(df['Text'][0])
+        df.head()
 
-    # leverage = re.findall('leverage\s*-*\s*(\d.*x)',text,re.IGNORECASE)
-    leverage = re.findall('[ ]*[-]*[ ]*(\d+)x',text,re.IGNORECASE)
-    # if (leverage): leverage = str(leverage[0]).split()
+        exchanges = 'Binance|kucoin|bitbuy'
+        conversion = 'usdt|usd|btc|eth'
+        exchangeregex = '|'.join(exchanges)
+        target = ('|').join(['1','2','3'])
+        # coins=''
+        def extractor(i):
+          text = re.sub('[\',\[\]]', '', i)
+          coin_name,conversion_name,call_exchange,call_type,call_duration,leverage,targets,stop_loss= ["None"]*8
+          if(re.search('free signal|entry point',text,re.IGNORECASE) and re.search('target|targets',text,re.IGNORECASE)):    
 
-    """some targets are not extracted although format is same """
-    targets=re.findall(r'Targets\s+(\d+(?:\.\d+)?(?:\s*-\s*\d+(?:\.\d+)?)+)',text,re.IGNORECASE)
-    if targets:
-      targets = [t.strip() for t in targets[0].split('-')]
-      
-    """ stop loss not used that much"""
-    stop_loss = re.findall('stop\s*loss\s*-*\s*(\d.\d*)',text,re.IGNORECASE)
+            coin_name=re.findall('(\w+){}'.format(conversion),text,re.IGNORECASE)
+            
+            conversion_name = re.findall('\w+({})'.format(conversion),str(text),re.IGNORECASE)
+            
+            """exchange not used"""
+            call_exchange = re.findall('\w+({})'.format(exchanges),text,re.IGNORECASE)
+            
+            call_type = re.findall('sell|buy',text,re.IGNORECASE)
 
-    
-  return (coin_name,conversion_name,call_exchange,call_type,call_duration,leverage,targets,stop_loss)
-  
-df["Call"] = df['Cleaned_text'].apply(extractor)
+            range =  re.findall(r'around\s+(\d+(?:\.\d+)?\s*-\s*\d+(?:\.\d+)?)',text,re.IGNORECASE)
+            """duration not used that much"""
+            call_duration = re.findall('long|short',text,re.IGNORECASE)
+            """current not given, between is given"""
+            # current_value=re.findall('')
 
-df1 = df.copy()
-df1 = df1.drop(columns=["Text"])
-df1.to_csv("../../FinalCallss/binancefuturesignal.csv", encoding='utf-8', index=False)
+            # leverage = re.findall('leverage\s*-*\s*(\d.*x)',text,re.IGNORECASE)
+            leverage = re.findall('[ ]*[-]*[ ]*(\d+)x',text,re.IGNORECASE)
+            # if (leverage): leverage = str(leverage[0]).split()
+
+            """some targets are not extracted although format is same """
+            targets=re.findall(r'Targets\s+(\d+(?:\.\d+)?(?:\s*-\s*\d+(?:\.\d+)?)+)',text,re.IGNORECASE)
+            if targets:
+              targets = [t.strip() for t in targets[0].split('-')]
+            
+            """ stop loss not used that much"""
+            stop_loss = re.findall('stop\s*loss\s*-*\s*(\d.\d*)',text,re.IGNORECASE)
+
+            
+          return (coin_name,conversion_name,call_exchange,call_type,call_duration,leverage,targets,stop_loss)
+        
+        df["Call"] = df['Cleaned_text'].apply(extractor)
+
+        df1 = df.copy()
+        df1 = df1.drop(columns=["Text"])
+
+
+        if not os.path.exists(f"{Root_PATH}\\shared\\Calls\\{folder_name}\\"):
+          os.mkdir(f"{Root_PATH}\\shared\\Calls\\{folder_name}")
+        
+        df1.to_csv(f"{Root_PATH}\\shared\\Calls\\{folder_name}/{folder_name}.csv", encoding='utf-8', index=False)
+
 
 
